1e3c98e790c1b479e5e40dd9bf151af9949a5f60
Carlos M <carlos.octavio.perez92@gmail.com>
2024-02-08 16:36:08 -0500

    test py files

--- tests/test_arrays_solution.py
+++ tests/test_arrays_solution.py
@@ -1,3 +1,12 @@
+import pytest
+
+import os, sys
+from pathlib import Path
+
+main_folder = str(Path.cwd().parent)
+sys.path.append(main_folder)
+
+
 from tutorial.arrays_solution import min_max
 
 
@@ -5,3 +14,6 @@ def test_min_max() -> None:
     assert min_max(range(10)) == (0, 9)
     assert min_max((7, 4, 2, 8, 1, 3)) == (1, 8)
     assert min_max([1]) == (1, 1)
+
+if __name__ == "__main__":
+    test_min_max()
